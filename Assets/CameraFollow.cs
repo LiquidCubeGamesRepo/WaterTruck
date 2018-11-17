3e33ebd35316bed9da8de93041faa2e1f4ca8139
Eram321 <pm4281@gmail.com>
2018-11-17 16:43:04 +0100

    Android build

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    [SerializeField] Transform target;
+    [SerializeField] float speed;
+
+    // Update is called once per frame
+	void Update () {
+        if (target)
+        {
+                var pos = target.position;
+                pos.z = transform.position.z;
+                transform.position = pos;
+            
+        }
+	}
+}
---
+++ Assets/Scenes/CarController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+
+    [SerializeField] float maxCarSpeed;
+    [SerializeField] float accelerationSpeed;
+    [SerializeField] WheelJoint2D[] wheels;
+
+    float carSpeed;
+
+    public UnityEngine.UI.Text text;
+
+    // Update is called once per frame
+    void Update () {
+
+#if UNITY_EDITOR
+        if (Input.GetButton("Accel"))
+            Acceleration();
+        else if (Input.GetButton("Break"))
+            Break();
+        else
+            NoInput();
+#elif UNITY_ANDROID
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            text.text = touch.position.ToString();
+            if (touch.position.x > Screen.width * 0.5f)
+                Acceleration();
+            else if(touch.position.x < Screen.width * 0.5f)
+                Break();
+        }
+        else
+            NoInput();
+#endif
+    }
+
+    private void NoInput()
+    {
+        foreach (var wheel in wheels){
+            wheel.useMotor = false;
+        }
+    }
+
+    private void Break()
+    {
+        foreach (var wheel in wheels){
+            var motor = wheel.motor;
+            motor.motorSpeed = 0;
+            wheel.motor = motor;
+            wheel.useMotor = true;
+        }
+    }
+
+    private void Acceleration()
+    {
+        carSpeed = Mathf.Clamp(carSpeed, 0, maxCarSpeed);
+        carSpeed += Time.deltaTime * accelerationSpeed;
+        foreach (var wheel in wheels)
+        {
+            var motor = wheel.motor;
+            motor.motorSpeed = -carSpeed;
+            wheel.motor = motor;
+            wheel.useMotor = true;
+        }
+    }
+}
